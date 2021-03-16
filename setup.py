487910214b685582f7eb9199a787af4c141b8f43
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2021-03-16 11:21:31 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='GVradar',
+    version='0.3',
+    author='Jason Pippitt',
+    author_email='jason.l.pippitt@nasa.gov',
+    packages=['', 'gvradar'],
+    scripts=[],
+    license='LICENSE.txt',
+    description='Dual Pol Quality Control and precipitation product package which utilizes the Python ARM Radar Toolkit and CSU Radar Tools',
+    long_description=open('README.rst').read(),
+)
