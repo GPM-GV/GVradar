1c10ee826f128efbe39484e0e4fc3b1883c64779
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2023-03-14 15:24:32 -0400

    Update plot_images.py

--- gvradar/plot_images.py
+++ gvradar/plot_images.py
@@ -111,7 +111,7 @@ def plot_fields_PPI(radar, sweep=0, fields=['CZ'], max_range=150, png=False, out
     display = pyart.graph.RadarMapDisplay(radar)
 
     num_fields = len(fields)
-    nrows = math.ceil((num_fields)//4)
+    nrows = math.ceil((num_fields)/4)
     print(num_fields)
     if nrows < 1 : nrows = 1
     if num_fields <= 4:
