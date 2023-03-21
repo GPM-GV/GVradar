861e599f086d00bad3f970bd48e1818deabd04fe
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2023-03-21 10:38:23 -0400

    Update plot_images.py

--- gvradar/plot_images.py
+++ gvradar/plot_images.py
@@ -296,8 +296,7 @@ def plot_fields_PPI(radar, sweep=0, fields=['CZ'], max_range=150, png=False, out
         ax = fig.add_subplot(spec[r_c[index]])
         display.plot_ppi(field, sweep=sweep, vmin=vmin, vmax=vmax, cmap=cmap, 
                          colorbar_label=units, mask_outside=True, title=title,
-                         lat_lines=np.arange(min_lat,max_lat,.5),
-                         lon_lines=np.arange(min_lon, max_lon, 1))
+                         axislabels_flag=False)
         display.set_limits(xlim=[-max_range,max_range], ylim=[-max_range,max_range])
 
         for rng in range(50,max_range+50,50):
