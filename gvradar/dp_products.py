7c4db436a6fb118d6a737dd6ef38ab4482b5a73a
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2023-05-16 09:03:10 -0400

    Update dp_products.py

--- gvradar/dp_products.py
+++ gvradar/dp_products.py
@@ -587,7 +587,7 @@ def mask_beyond_150(self):
             fields.append(fld)
     for fld in fields:
         nf = self.radar.fields[fld]['data']
-        nf[apply_beyond] = -32767.0
+        nf[apply_beyond] = -9999.0
         #if fld in sfields: nf = np.ma.filled(nf, fill_value=-32767.0)
         self.radar.add_field_like(fld,fld,nf,replace_existing=True)
 
