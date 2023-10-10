31d6aabf832fd30485cb872706b6403f4d663a65
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2023-10-10 13:00:18 -0400

    Update dpqc.py

--- gvradar/dpqc.py
+++ gvradar/dpqc.py
@@ -818,9 +818,11 @@ def calculate_kdp(self):
     if self.site in std_list:
         window=4
         std_gate=7
+        nfilter=2
     else:
         window=4
         std_gate=15
+        nfilter=1
 
     # Range needs to be supplied as a variable, with same shape as DZ
     rng2d, az2d = np.meshgrid(self.radar.range['data'], self.radar.azimuth['data'])
@@ -828,7 +830,8 @@ def calculate_kdp(self):
 
     KDPB, PHIDPB, STDPHIB = csu_kdp.calc_kdp_bringi(dp=DP, dz=DZ, rng=rng2d/1000.0, 
                                                     thsd=25, gs=gate_spacing, 
-                                                    window=window, nfilter=1, std_gate=std_gate)
+                                                    window=window, nfilter=nfilter, 
+                                                    std_gate=std_gate)
 
     self.radar = cm.add_field_to_radar_object(KDPB, self.radar, field_name='KD', 
 		units='deg/km',
