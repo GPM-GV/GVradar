a01202ef470c26acf50cb663a3add60df054d7ca
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2023-12-20 09:39:24 -0500

    Update dpqc.py

--- gvradar/dpqc.py
+++ gvradar/dpqc.py
@@ -841,7 +841,7 @@ def calculate_kdp(self):
 #    DZ = self.radar.fields[self.ref_field_name]['data'].copy()
 #    DP = self.radar.fields[self.phi_field_name]['data'].copy()
 
-    std_list  = ['AL1','JG1','MC1','NT1','PE1','SF1','ST1','SV1','TM1','NPOL']
+    std_list  = ['AL1','JG1','MC1','NT1','PE1','SF1','ST1','SV1','TM1']
     if self.site in std_list:
         DZ = cm.extract_unmasked_data(self.radar, self.ref_field_name)
         DP = cm.extract_unmasked_data(self.radar, self.phi_field_name)
