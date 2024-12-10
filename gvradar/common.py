c31fc22894e29ed1d1c1b22f461bcc1abfc5f6a5
jlpippitt <66760569+jlpippitt@users.noreply.github.com>
2024-12-10 10:51:16 -0500

    Update common.py

--- gvradar/common.py
+++ gvradar/common.py
@@ -58,7 +58,7 @@ def get_ruc_sounding(self):
     dewpoint_c = sound.DWPT.values
 
     mydata=dict(zip(('hght','pres','temp','dwpt'),(height_m,presssure_pa,temperature_c,dewpoint_c)))
-    print(mydata)
+    print(sound)
     sounding=SkewT.Sounding(soundingdata=mydata)
     #print(sounding)       
     radar_T, radar_z = interpolate_sounding_to_radar(sounding, self.radar)
